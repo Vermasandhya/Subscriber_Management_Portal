533da9aa9b26f95b07be245a5835543ddfc658d2
HariniElumalai03 <harini.elumalai17@gmail.com>
2025-08-20 17:35:44 +0530

    Delete backend/CallHistory directory

--- backend/CallHistory/src/main/java/com/CallHistory/CallHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.CallHistory;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CallHistoryRepository extends JpaRepository<CallHistory, Long> {
-    List<CallHistory> findByCallerNumber(String callerNumber);
-}
